b6519559927214a38325843ed1b5c4fba006d001
Parul Singh <parul.rhl@gmail.com>
2016-06-23 13:05:04 +0100

    Fix indentation

--- Ruby/lesson_03.rb
+++ Ruby/lesson_03.rb
@@ -1,17 +1,16 @@
 # Ruby day 3
 # if, else & elsif 
+
 puts "Enter a = "
 a = gets.chomp
+
 puts "Enter b = "
 b = gets.chomp
 
 if a > b
   print "a is greater than b"
-  
 elsif a < b
-     print "b is greater than a"
-    
+	print "b is greater than a"
 else a = b
-    print " a is equal to b"
-    
+	print " a is equal to b"
 end    
