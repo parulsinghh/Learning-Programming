8e7d00c4251517eaad2c263b017e2193f7767df4
Parul Singh <parul.rhl@gmail.com>
2016-06-23 12:55:24 +0100

    if, else & elsif

---
+++ Ruby/lesson_03.rb
@@ -0,0 +1,17 @@
+# Ruby day 3
+# if, else & elsif 
+puts "Enter a = "
+a = gets.chomp
+puts "Enter b = "
+b = gets.chomp
+
+if a > b
+  print "a is greater than b"
+  
+elsif a < b
+     print "b is greater than a"
+    
+else a = b
+    print " a is equal to b"
+    
+end    
