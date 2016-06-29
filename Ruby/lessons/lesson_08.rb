0528b4e7ac4e55f05fceb95977ffc5289e153e75
Parul Singh <parul.rhl@gmail.com>
2016-06-29 04:30:53 -0800

    histogram

---
+++ Ruby/lessons/lesson_08.rb
@@ -0,0 +1,22 @@
+# Ruby day 8
+# Histogram
+
+puts "Text please: "
+text = gets.chomp
+words = text.split("")
+frequencies = Hash.new(0)
+words.each { |word| frequencies[word] += 1 }
+frequencies = frequencies.sort_by { |word, frequency| frequency }
+frequencies.reverse!
+frequencies.each { |word, frequency| puts "#{word} - #{frequency}" }
+
+
+
+
+
+
+
+
+
+
+
