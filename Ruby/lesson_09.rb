c3ef11d0cb1860baede0e6ef3a104255e005383f
Parul Singh <parul.rhl@gmail.com>
2016-07-01 19:04:58 +0800

    Building own method

---
+++ Ruby/lesson_09.rb
@@ -0,0 +1,15 @@
+# Ruby day 9
+# Building own method
+
+def alphabetize(arr, rev=false)
+  arr.sort!
+  if rev == true
+   arr.reverse!
+  else 
+   arr
+  end
+end
+
+numbers = [40, 55, 79, 12, 29]
+
+puts alphabetize(numbers)
