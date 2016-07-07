a0588f4f1bfec0dc5b45844afc47bf4e210d1dfd
Parul Singh <parul.rhl@gmail.com>
2016-07-07 10:09:53 +0100

    Fix indentation

--- Ruby/lesson_11.rb
+++ Ruby/lesson_11.rb
@@ -2,10 +2,10 @@
 # Small app
 
 movies = {
-    Up: 9,
-    Frozen: 8,
-    Tangle: 7,
-    Finding_nemo: 9
+  Up: 9,
+  Frozen: 8,
+  Tangle: 7,
+  Finding_nemo: 9
 }
 
 puts "What would you like to do?"
@@ -19,47 +19,43 @@ choice = gets.chomp
 case choice
 
 when "add"
-
-puts "What movie you want to add"
- title = gets.chomp.to_sym
+  puts "What movie you want to add"
+  title = gets.chomp.to_sym
   if movies[title.to_sym].nil?
-   puts "What is the rating"
- rating = gets.chomp.to_i
- movies[title] = rating
- puts "#{title} is has been added and the rating is #{rating}"
-
-else
+    puts "What is the rating"
+    rating = gets.chomp.to_i
+    movies[title] = rating
+    puts "#{title} is has been added and the rating is #{rating}"
+  else
     puts "Movie already exists"
-end
-    
-when "update"
+  end
 
-puts "Which movie you want to update"
-title = gets.chomp.to_sym
-if movies[title].nil?
-  puts "Movie dont exist"
-else
-  puts "New rating"
-  rating = gets.chomp.to_i
-  movies[title] = rating
-  puts "#{title} has been updated with new rating of #{rating}."
-end
+when "update"
+  puts "Which movie you want to update"
+  title = gets.chomp.to_sym
+  if movies[title].nil?
+    puts "Movie dont exist"
+  else
+    puts "New rating"
+    rating = gets.chomp.to_i
+    movies[title] = rating
+    puts "#{title} has been updated with new rating of #{rating}."
+  end
 
 when "display"
-
-movies.each{|movie, rating| puts "#{movie}: #{rating}"}
- 
-when "delete"
-
- puts "What movie you want to delete"
- title = gets.chomp.to_sym
- if movies[title].nil?
-    puts "Movie dont exist"
-else 
-  movies.delete(title.to_sym)
-  puts "#{title} has been removed."
-end
+  movies.each do |movie, rating| 
+    puts "#{movie}: #{rating}"}
+  end
   
+when "delete"
+  puts "What movie you want to delete"
+  title = gets.chomp.to_sym
+  if movies[title].nil?
+    puts "Movie dont exist"
+  else 
+    movies.delete(title.to_sym)
+    puts "#{title} has been removed."
+  end
 else 
- puts "Error!"
+  puts "Error!"
 end
