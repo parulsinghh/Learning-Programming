d0754137f29b5755ee80c0659909cb5c640dfa82
Parul Singh <parul.rhl@gmail.com>
2016-07-13 19:13:37 -0800

    Learn ruby class

---
+++ Ruby/lesson_16.rb
@@ -0,0 +1,22 @@
+# Ruby lesson 16
+# Class
+
+class Person
+  attr_writer :first_name, :last_name
+  
+  def initialize(first_name, last_name)
+    @first_name = first_name
+    @last_name = last_name
+  end
+
+  def description
+  puts "My full name is #{@first_name} #{@last_name}."
+  end
+end
+
+name1 = Person.new("Eric", "Stone")
+name2 = Person.new("James", "Baker")
+
+name1.description
+name2.description
+
