741675a5f9a5b2ad623a69faeafa2aaec40e29bb
Parul Singh <parul.rhl@gmail.com>
2016-07-14 21:38:00 -0800

    Globel variable

---
+++ Ruby/lesson_17.rb
@@ -0,0 +1,27 @@
+# Ruby lesson 17
+# Inheritance, Globel variable
+
+class Message
+  @@messages_sent = 0
+  
+  def initialize(from, to)
+    @from = from
+    @to = to
+    @@messages_sent += 1
+    messages_sent
+  end 
+  
+  def messages_sent
+    puts "#{@@messages_sent}. Message sent from #{@from} to #{@to}"
+  end
+end
+
+class Email < Message
+    def initialize(from, to)
+      super
+    end
+ end
+
+my_message = Message.new("Roxy", "Lucy")
+my_message = Message.new("Eric", "James")
+
