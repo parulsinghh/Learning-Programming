ca26f2be4c38ead6efda10e80a6b61f9124968b3
Parul Singh <parul.rhl@gmail.com>
2016-07-18 11:04:29 +0100

    Learn about classes in ruby

---
+++ Ruby/lesson_19.rb
@@ -0,0 +1,29 @@
+# Ruby lesson 19
+
+class Computer
+  @@users = {}
+  def initialize(username, password)
+    @username = username
+    @password = password
+    @@users[username] = password
+    @files = {}
+  end
+  
+  def create (filename)
+    time = Time.now
+    @files[filename] = time
+    puts "#{filename} was created by #{@username} at #{time}."
+  end
+  
+  def Computer.get_users
+    @@users
+  end
+end
+
+my_computer = Computer.new("Erica", 01234)
+your_computer = Computer.new("Jessi", 97631)
+
+my_computer.create("daily_plans.txt")
+your_computer.create("todo.txt")
+
+puts "Users: #{Computer.get_users}"
